a31a40026ea434732b209ff2b27c06c85c25235d
Wilson Oh <e0773510@u.nus.edu>
2022-12-03 02:17:38 +0800

    further clean up day2 code by implementing FromStr and PartialOrd traits for Move enum; Use math trick to find out which side is the winner in a short and concise manner

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,109 +1,101 @@
-#![allow(non_snake_case, dead_code)]
+use std::{cmp::Ordering, str::FromStr};
 
-use std::collections::HashMap;
-
-/*
-const EXAMPLE: &str = r#"A Y
-B X
-C Z"#;
-*/
-
-#[derive(Hash, PartialEq, Eq, Debug, Copy, Clone)]
 enum State {
-    Win,
-    Lose,
-    Draw,
+    Win = 6,
+    Lose = 0,
+    Draw = 3,
 }
 
-fn get_match_result(a: &char, b: &char) -> State {
-    if *a == *b {
-        return State::Draw;
-    }
-    if *b == 'A' {
-        if *a == 'B' {
-            return State::Lose;
-        }
-        if *a == 'C' {
-            return State::Win;
-        }
-    }
-    if *b == 'B' {
-        if *a == 'A' {
-            return State::Win;
-        }
-        if *a == 'C' {
-            return State::Lose;
-        }
-    }
-    if *a == 'A' {
-        return State::Lose;
-    }
-    return State::Win;
+#[derive(PartialEq, Copy, Clone)]
+enum Move {
+    Rock = 0,
+    Paper = 1,
+    Scissors = 2,
 }
 
-fn get_shape_needed(state: &State, a: &char) -> char {
+impl FromStr for Move {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Move::Rock),
+            "B" | "Y" => Ok(Move::Paper),
+            "C" | "Z" => Ok(Move::Scissors),
+            _ => Err("invalid move".to_string()),
+        }
+    }
+}
+
+impl FromStr for State {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(State::Lose),
+            "Y" => Ok(State::Draw),
+            "Z" => Ok(State::Win),
+            _ => Err("invalid move".to_string()),
+        }
+    }
+}
+
+impl PartialOrd for Move {
+    /// Use some math trick to easily calculate who wins
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if *self as u8 == *other as u8 {
+            return Some(Ordering::Equal);
+        }
+        if (*self as u8 + 1) % 3 == *other as u8 {
+            return Some(Ordering::Less);
+        }
+        Some(Ordering::Greater)
+    }
+}
+
+fn get_shape_needed(state: &State, enemy_move: &Move) -> Move {
     match state {
-        State::Win => {
-            if *a == 'A' {
-                return 'B';
-            }
-            if *a == 'B' {
-                return 'C';
-            }
-            return 'A';
-        }
-        State::Lose => {
-            if *a == 'A' {
-                return 'C';
-            }
-            if *a == 'B' {
-                return 'A';
-            }
-            return 'B';
-        }
-        State::Draw => {
-            return a.clone();
-        }
+        State::Win => match enemy_move {
+            Move::Rock => Move::Paper,
+            Move::Paper => Move::Scissors,
+            Move::Scissors => Move::Rock,
+        },
+        State::Lose => match enemy_move {
+            Move::Rock => Move::Scissors,
+            Move::Paper => Move::Rock,
+            Move::Scissors => Move::Paper,
+        },
+        State::Draw => enemy_move.clone(),
     }
 }
 
 fn part_one(input: &str) -> u32 {
-    let shape_score: HashMap<char, u32> = HashMap::from([('A', 1), ('B', 2), ('C', 3)]);
-    let shape_map: HashMap<char, char> = HashMap::from([('X', 'A'), ('Y', 'B'), ('Z', 'C')]);
-    let score_map: HashMap<State, u32> =
-        HashMap::from([(State::Win, 6), (State::Draw, 3), (State::Lose, 0)]);
     input.lines().fold(0, |acc: u32, curr| {
-        let tmp: Vec<char> = curr
+        let tmp: Vec<Move> = curr
             .split_whitespace()
-            .map(|s| s.chars().next().unwrap())
+            .map(|s| s.parse::<Move>().unwrap())
             .collect();
-        let a = tmp[0];
-        let b = shape_map[&tmp[1]];
-        acc + shape_score[&b] + score_map[&get_match_result(&a, &b)]
+        acc + match tmp[0].partial_cmp(&tmp[1]) {
+            Some(Ordering::Less) => State::Win,
+            Some(Ordering::Equal) => State::Draw,
+            Some(Ordering::Greater) => State::Lose,
+            None => unreachable!(),
+        } as u32
+            + tmp[1] as u32
+            + 1 // add one since the problem states that the moves are 1-indexed
     })
 }
 
 fn part_two(input: &str) -> u32 {
-    let state_map: HashMap<char, State> =
-        HashMap::from([('X', State::Lose), ('Y', State::Draw), ('Z', State::Win)]);
-    let score_map: HashMap<State, u32> =
-        HashMap::from([(State::Win, 6), (State::Draw, 3), (State::Lose, 0)]);
-    let shape_score: HashMap<char, u32> = HashMap::from([('A', 1), ('B', 2), ('C', 3)]);
     input.lines().fold(0, |acc: u32, curr| {
-        let tmp: Vec<char> = curr
-            .split_whitespace()
-            .map(|s| s.chars().next().unwrap())
-            .collect();
-        let a = tmp[0];
-        let state = state_map[&tmp[1]];
-        let shape = get_shape_needed(&state, &a);
-        acc + shape_score[&shape] + score_map[&state]
+        let tmp: Vec<&str> = curr.split_whitespace().collect();
+        let enemy_move: Move = tmp[0].parse().unwrap();
+        let state: State = tmp[1].parse().unwrap();
+        let my_move = get_shape_needed(&state, &enemy_move);
+        acc + my_move as u32 + state as u32 + 1
     })
 }
 
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
-    let ans = part_two(&input);
+    let ans = part_one(&input);
     println!("{ans}");
     Ok(())
 }
