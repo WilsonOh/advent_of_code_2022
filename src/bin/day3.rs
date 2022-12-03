fb58ea8ddba860130679ccaaa6f4ff37a4c5e150
Wilson Oh <e0773510@u.nus.edu>
2022-12-03 23:59:08 +0800

    added day3

---
+++ src/bin/day3.rs
@@ -0,0 +1,45 @@
+use anyhow::Result;
+use itertools::Itertools;
+use std::collections::HashSet;
+use std::fs;
+
+fn part_one(input: &str) -> u32 {
+    input.lines().fold(0, |acc: u32, curr| {
+        let first: HashSet<char> = (&curr[..curr.len() / 2]).chars().collect();
+        let second: HashSet<char> = (&curr[curr.len() / 2..]).chars().collect();
+        let common_char = *first.intersection(&second).next().unwrap();
+        let val = if common_char.is_lowercase() {
+            ((common_char as u8) - ('a' as u8)) + 1
+        } else {
+            ((common_char as u8) - ('A' as u8)) + 27
+        };
+        acc + val as u32
+    })
+}
+
+fn part_two(input: &str) -> u32 {
+    input
+        .lines()
+        .collect_vec()
+        .chunks(3)
+        .fold(0, |acc: u32, curr| {
+            let first: HashSet<char> = curr[0].chars().collect();
+            let second: HashSet<char> = curr[1].chars().collect();
+            let third: HashSet<char> = curr[2].chars().collect();
+            let tmp: HashSet<_> = first.intersection(&second).copied().collect();
+            let common_char = *tmp.intersection(&third).next().unwrap();
+            let val = if common_char.is_lowercase() {
+                ((common_char as u8) - ('a' as u8)) + 1
+            } else {
+                ((common_char as u8) - ('A' as u8)) + 27
+            };
+            acc + val as u32
+        })
+}
+
+fn main() -> Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+    let ans = part_two(&input);
+    println!("{ans}");
+    Ok(())
+}
