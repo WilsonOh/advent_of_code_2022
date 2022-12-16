324ef2863a0e374563e70f1ca2489d7e7cbf90c2
Wilson Oh <e0773510@u.nus.edu>
2022-12-17 00:28:27 +0800

    added day16.rs; part two took 9+ mins to run...the bottleneck is using a HashMap for as a DP table instead of a plain Vec

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,13 +1,16 @@
 use anyhow::Result;
 use itertools::Itertools;
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap, HashSet},
+    hash::{Hash, Hasher},
+};
 
 fn parse_graph(input: &str) -> (HashMap<String, Vec<String>>, HashMap<String, u32>) {
     let mut graph: HashMap<String, Vec<String>> = HashMap::new();
     let mut flow_rates: HashMap<String, u32> = HashMap::new();
     let re = Regex::new(
-        r"Valve (?P<node>[[:upper:]]{2}) has flow rate=(?P<flow_rate>\d+); tunnels? leads? to valves? (?P<neighbours>.*)",
+        r"Valve (?P<node>[[:upper:]]{2}) has flow rate=(?P<flow_rate>\d+); tunnels? leads? to valves? (?P<neighbours>[, A-Z]*)",
     )
     .unwrap();
     for line in input.lines() {
@@ -24,58 +27,103 @@ fn parse_graph(input: &str) -> (HashMap<String, Vec<String>>, HashMap<String, u3
     (graph, flow_rates)
 }
 
+fn hash_strings(opened: &HashSet<String>) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    opened.iter().fold(0, |acc, curr| {
+        curr.hash(&mut hasher);
+        acc ^ hasher.finish()
+    })
+}
+
 fn dfs(
     adj_list: &HashMap<String, Vec<String>>,
     opened: &mut HashSet<String>,
     curr_node: String,
     flow_rates: &HashMap<String, u32>,
-    time_left: u32,
-) -> u32 {
-    if time_left <= 0 {
-        return 0;
-    }
-    let mut best = 0;
-    if !opened.contains(&curr_node) {
-        let curr_pressure = (time_left - 1) * flow_rates[&curr_node];
-        for nei in &adj_list[&curr_node] {
-            if curr_pressure != 0 {
-                opened.insert(curr_node.clone());
-            }
-            best = best.max(
-                curr_pressure
-                    + dfs(
-                        adj_list,
-                        opened,
-                        nei.to_string(),
-                        flow_rates,
-                        if curr_pressure == 0 {
-                            time_left - 1
-                        } else {
-                            time_left - 2
-                        },
-                    ),
-            );
-            opened.remove(&curr_node);
-        }
-    } else {
-        for nei in &adj_list[&curr_node] {
-            best = best.max(dfs(
+    time_left: i32,
+    dp: &mut HashMap<(String, u64, i32, u32), i32>,
+    elephants: u32,
+) -> i32 {
+    if time_left == 0 {
+        return if elephants > 0 {
+            dfs(
                 adj_list,
                 opened,
-                nei.to_string(),
+                "AA".to_string(),
                 flow_rates,
-                time_left - 1,
-            ));
-        }
+                26,
+                dp,
+                elephants - 1,
+            )
+        } else {
+            0
+        };
     }
+    let state = (
+        curr_node.clone(),
+        hash_strings(opened),
+        time_left,
+        elephants,
+    );
+    if dp.contains_key(&state) {
+        return dp[&state];
+    }
+    let mut best = 0;
+    if !opened.contains(&curr_node) && flow_rates[&curr_node] > 0 {
+        let curr_pressure = (time_left - 1) * (flow_rates[&curr_node] as i32);
+        opened.insert(curr_node.clone());
+        best = best.max(
+            curr_pressure
+                + dfs(
+                    adj_list,
+                    opened,
+                    curr_node.clone(),
+                    flow_rates,
+                    time_left - 1,
+                    dp,
+                    elephants,
+                ),
+        );
+        opened.remove(&curr_node);
+    }
+    for nei in &adj_list[&curr_node] {
+        best = best.max(dfs(
+            adj_list,
+            opened,
+            nei.to_string(),
+            flow_rates,
+            time_left - 1,
+            dp,
+            elephants,
+        ));
+    }
+    dp.insert(state, best);
     best
 }
 
 fn main() -> Result<()> {
-    let input = std::fs::read_to_string("sample.txt")?;
+    let input = std::fs::read_to_string("input.txt")?;
     let (adj_list, flow_rates) = parse_graph(&input);
+    let mut dp = HashMap::new();
     let mut opened = HashSet::new();
-    let ans = dfs(&adj_list, &mut opened, "AA".to_string(), &flow_rates, 30);
-    println!("part one: {ans}");
+    let part_one = dfs(
+        &adj_list,
+        &mut opened,
+        "AA".to_string(),
+        &flow_rates,
+        30,
+        &mut dp,
+        0,
+    );
+    /* let part_two = dfs(
+        &adj_list,
+        &mut opened,
+        "AA".to_string(),
+        &flow_rates,
+        26,
+        &mut dp,
+        1,
+    ); */
+    println!("part one: {part_one}");
     Ok(())
 }
